3bda681c4e2d2a4255c0a4a4a407fb1b3c0068a9
3x3y3z3t <recette.lemongrass@yahoo.com.vn>
2022-07-17 02:04:16 +0700

    Add support for other custom thruster blocks (advanced)
    - Added support for custom thruster blocks from other mods
    - Moved GameLogic class declaration to a separated file for easier setup
    - Removed mod check
    - Refactored for Config and Logger

---
+++ Data/Scripts/AtmoHydroPower/AtmoHydroPower_GameLogic_Setup.cs
@@ -0,0 +1,37 @@
+﻿/*  AtmoHydroPower_GameLogic_Setup.cs
+ *  Version: v1.0 (2022.07.17)
+ * 
+ * GameLogic component for AtmoHydroPower mod.
+ * 
+ * This file contains the setup part for GameLogic class.
+ * I put this part into a separated file so modders (especially certain someone) 
+ *  don't have to worry about the main code part (that they will not touch anyway)
+ *  and can easily see where they need to edit so AHP works with their mod.
+ *  Yes, everything you need to edit is in this file.
+ *  
+ * How to edit (see README.md for setup guide)
+ *      1. Put your thruster blocks' SubtypeID in the string array, same as the two example I placed.
+ *      2. Remove the example if you mind.
+ * 
+ *  Contributor
+ *      Arime-chan
+ */
+
+using Sandbox.Common.ObjectBuilders;
+using VRage.Game.Components;
+
+namespace AtmoHydroPower
+{
+    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_Thrust), false, new string[] {
+        /* Place your thrusters' SubtypeIDs below this line. */
+
+        "LargeBlockExampleThruster", // this is an example;
+        "SmallBlockExampleThruster", // this is an example;
+
+
+
+        /* Place your thrusters' SubtypeIDs above this line. */
+    })]
+    partial class AtmoHydroPower_GameLogic : MyGameLogicComponent
+    { }
+}
